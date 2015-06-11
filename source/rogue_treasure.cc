7f9f9700ad2021f5bf65e3d48a3c4c9a09fa1778
Aaron <abatilo@uccs.edu>
2015-06-10 21:51:51 -0600

    Added minimal border collision

--- source/rogue_treasure.cc
+++ source/rogue_treasure.cc
@@ -15,21 +15,74 @@ void PrintMap(vector<string> &map) {
   }
 }
 
+bool ValidLocation(vector<string> &map, POS &pos) {
+  int x = pos.x;
+  int y = pos.y;
+
+  //Checks for out of bounds
+  if (x < 0 || x > map[y].size() - 1) {
+    return false;
+  }
+  else if (y < 0 || y > map.size() - 1) {
+    return false;
+  }
+
+  //Check for a wall
+  if (map[y][x] == '|') {
+    return false;
+  }
+
+  return true;
+}
+
 void MoveCharacter(vector<string> &map, string action, POS &pos) {
   char action_char = tolower(action[0]);
+
   if (action_char == 'r') {
+    POS nextPos;
+    nextPos.x = pos.x + 1;
+    nextPos.y = pos.y;
+
+    if (!ValidLocation(map, nextPos)) {
+      return;
+    }
+
     map[pos.y][pos.x++] = ' ';
     map[pos.y][pos.x] = '@';
   } 
   else if (action_char == 'd') {
+    POS nextPos;
+    nextPos.x = pos.x;
+    nextPos.y = pos.y + 1;
+
+    if (!ValidLocation(map, nextPos)) {
+      return;
+    }
+
     map[pos.y++][pos.x] = ' ';
     map[pos.y][pos.x] = '@';
   } 
   else if (action_char == 'u') {
+    POS nextPos;
+    nextPos.x = pos.x;
+    nextPos.y = pos.y - 1;
+
+    if (!ValidLocation(map, nextPos)) {
+      return;
+    }
+
     map[pos.y--][pos.x] = ' ';
     map[pos.y][pos.x] = '@';
   }
   else if (action_char == 'l') {
+    POS nextPos;
+    nextPos.x = pos.x - 1;
+    nextPos.y = pos.y;
+
+    if (!ValidLocation(map, nextPos)) {
+      return;
+    }
+
     map[pos.y][pos.x--] = ' ';
     map[pos.y][pos.x] = '@';
   }
